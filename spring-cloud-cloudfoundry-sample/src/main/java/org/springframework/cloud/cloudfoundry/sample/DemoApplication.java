898202321dfcdcd12998c5299cc5966ca25cbbd5
Spencer Gibb <spencer@gibb.us>
2015-05-11 10:27:50 -0600

    Got s-c-cf-sample working with discovery. Organized dependencies so they use a consistent s-c version. Moved DemoApplication out of o.s.c.cf package to avoid @ComponentScan problems

--- spring-cloud-cloudfoundry-sample/src/main/java/org/springframework/cloud/cloudfoundry/sample/DemoApplication.java
+++ spring-cloud-cloudfoundry-sample/src/main/java/org/springframework/cloud/cloudfoundry/sample/DemoApplication.java
@@ -1,24 +1,36 @@
-package org.springframework.cloud.cloudfoundry;
+package org.springframework.cloud.cloudfoundry.sample;
 
 import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.cloudfoundry.discovery.EnableCloudFoundryClient;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @SpringBootApplication
-@EnableCloudFoundryClient
+@EnableDiscoveryClient
 @RestController
 public class DemoApplication {
+	@Autowired
+	DiscoveryClient discoveryClient;
+
+	@RequestMapping("/")
+	public List<String> home() {
+		return discoveryClient.getServices();
+	}
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
 
-    @Bean
+    /*@Bean
     CommandLineRunner runner(final DiscoveryClient discoveryClient) {
         return new CommandLineRunner() {
             @Override
@@ -28,6 +40,6 @@ public class DemoApplication {
                 }
             }
         };
-    }
+    }*/
 }
 
